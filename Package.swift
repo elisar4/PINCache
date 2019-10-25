928a26a30a9c5f6e746cdef509a1d1a7a2010520
Vladimir <bobah1991@gmail.com>
2019-10-25 06:00:59 +0300

    remove all tagrets except ios

--- Package.swift
+++ Package.swift
@@ -6,10 +6,7 @@ import PackageDescription
 let package = Package(
     name: "PINCache",
     platforms: [
-        .macOS(.v10_10),
-        .iOS(.v8),
-        .watchOS(.v2),
-        .tvOS(.v9)
+        .iOS(.v8)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -19,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"), 
+        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/3a4oT/PINOperation.git", .branch("swiftPM"))
     ],
     targets: [
