5bce9fa3c0408f139b9976f91805614f00341dfc
Vladimir <bobah1991@gmail.com>
2019-10-25 05:46:13 +0300

    remove .build folder

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        // .package(url: /* package url */, from: "1.0.0"), 
         .package(url: "https://github.com/3a4oT/PINOperation.git", .branch("swiftPM"))
     ],
     targets: [
